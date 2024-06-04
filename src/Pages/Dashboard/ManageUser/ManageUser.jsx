33084f0b85126c7306605d16a6ceaab69dd286ad
Maruf Hossain <mdmarufh974@gmail.com>
2024-06-05 02:05:14 +0600

    user block and unblock function added

--- src/Pages/Dashboard/ManageUser/ManageUser.jsx
+++ src/Pages/Dashboard/ManageUser/ManageUser.jsx
@@ -1,4 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
+import toast, { Toaster } from "react-hot-toast";
+import { FaTrashAlt } from "react-icons/fa";
 import Swal from 'sweetalert2';
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 
@@ -24,34 +26,54 @@ const ManageUser = () => {
             confirmButtonColor: "#d33",
             cancelButtonColor: "#3085d6",
             confirmButtonText: "Yes, delete it!"
-          }).then((result) => {
+        }).then((result) => {
             if (result.isConfirmed) {
                 // User delete from database
                 axiosSecure.delete(`/users/${user._id}`)
-                .then(res=>{
-                    if(res.data.deletedCount){
-                        Swal.fire({
-                            title: "Deleted!",
-                            text: "Your file has been deleted.",
-                            icon: "success"
-                          });
-                          refetch()
-                    }
-                })
+                    .then(res => {
+                        if (res.data.deletedCount) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                            refetch()
+                        }
+                    })
             }
-          });
+        });
     }
 
     // make creator
-    const handleCreator = user =>{
+    const handleCreator = user => {
         axiosSecure.patch(`/users/creators/${user._id}`)
-        .then(res=>{            
-            if(res.data.modifiedCount > 0){
-                alert('creator makeing done')
-                refetch()
-            }
-        })
-        
+            .then(res => {
+                if (res.data.modifiedCount > 0) {
+                    toast.success('WOW! Creator making done')
+                    refetch()
+                }
+            })
+    }
+
+    // block user
+    const handleBlockUser = user => {
+        axiosSecure.patch(`/users/block/${user._id}`)
+            .then(res => {                
+                if (res.data.modifiedCount > 0) {
+                    toast.success('This user blocked now')
+                    refetch();
+                }
+            })
+    }
+    // Unblock user
+    const handleUnBlockUser = user => {
+        axiosSecure.patch(`/users/unblock/${user._id}`)
+            .then(res => {                
+                if (res.data.modifiedCount > 0) {
+                    toast.success('This user blocked now')
+                    refetch();
+                }
+            })
     }
 
     return (
@@ -72,15 +94,18 @@ const ManageUser = () => {
                     {/* body */}
                     <tbody>
                         {
-                            users.map((user, index )=> <tr className="hover" key={user._id}>
-                                <th>{index+1}</th>
+                            users.map((user, index) => <tr className="hover" key={user._id}>
+                                <th>{index + 1}</th>
                                 <td>{user.name}</td>
                                 <td>{user.email}</td>
+                                
                                 <td>
-                                    {user.role ? user.role : <button onClick={()=>handleCreator(user)}>Make Creator</button>}
+                                    {user.role ? user.role : <button onClick={() => handleCreator(user)}>Make Creator</button>}
                                 </td>
-                                <td><button >Block</button></td>
-                                <td><button onClick={()=>handleDelete(user)}>Delete</button></td>
+                                <td>
+                                    {user.status === 'block' ? <button onClick={() => handleUnBlockUser(user)} >Unblock</button> : <button onClick={() => handleBlockUser(user)} >Block</button>}
+                                </td>
+                                <td><button onClick={() => handleDelete(user)}><FaTrashAlt className="ml-5 text-red-500" /></button></td>
                             </tr>)
                         }
                     </tbody>
@@ -89,6 +114,10 @@ const ManageUser = () => {
                     users.length === 0 ? <p className="text-center py-10">You have not any user.</p> : ""
                 }
             </div>
+            <Toaster
+                position="top-center"
+                reverseOrder={false}
+            />
         </div>
     );
 };
