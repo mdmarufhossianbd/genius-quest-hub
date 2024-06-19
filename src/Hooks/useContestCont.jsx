b2e9a8727c8230d480c405f3a468943794d1b94c
mdmarufhossainbd <mdmarufh974@gmail.com>
2024-06-19 15:28:17 +0600

    pagination added in contest page

---
+++ src/Hooks/useContestCont.jsx
@@ -0,0 +1,18 @@
+import { useQuery } from '@tanstack/react-query';
+import useAxiosPublic from './useAxiosPublic';
+
+const useContestCont = () => {
+    const axiosPublic = useAxiosPublic();
+    const {data : contestCount = {}, isLoading, refetch} = useQuery({
+        queryKey: ['contestCount'],
+        queryFn: async () => {
+            const res = await axiosPublic.get('/total-contest')
+            console.log(res.data);
+            return res.data;
+        }
+    });
+
+    return [contestCount, isLoading, refetch]
+};
+
+export default useContestCont;
--- src/Pages/Contest/Contest.jsx
+++ src/Pages/Contest/Contest.jsx
@@ -1,22 +1,76 @@
-import useContest from "../../Hooks/useContest";
+import { useEffect, useState } from "react";
 import ContestCart from "./ContestCart";
+import useAxiosPublic from "../../Hooks/useAxiosPublic";
+import { useQuery } from "@tanstack/react-query";
 
 const Contest = () => {
-    const [contests, isLoading] = useContest();
-    const publishContest = contests.filter(item => item.contestStatus == 'Publish')
-    if(isLoading){
+    const axiosPublic = useAxiosPublic();
+    const itemPerPage = 10;
+    const [currentPage, setCurrentPage] = useState(0);
+    const [contestCount, setContestCount] = useState(0);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/total-contest')
+            .then(res => res.json())
+            .then(data => {
+                setContestCount(data.count)
+            })
+    }, [])
+
+    const { data: contests = [], isLoading, refetch } = useQuery({
+        queryKey: ['contests'],
+        queryFn: async () => {
+            const res = await axiosPublic.get(`/contests?page=${currentPage}&size=${itemPerPage}`)
+            return res.data;
+        }
+    });
+    const publishContest = contests?.filter(item => item.contestStatus === 'Publish')
+    const numberOfPages = Math.ceil(contestCount / itemPerPage)
+    const pages = [...Array(numberOfPages).keys()]
+
+    if (isLoading) {
         return <div className="flex justify-center items-center min-h-screen">
-        <span className=" loading loading-dots loading-lg"></span>
-    </div>
+            <span className=" loading loading-dots loading-lg"></span>
+        </div>
     }
+
+    const handlePrev = () => {
+        if (currentPage > 0) {
+            setCurrentPage(currentPage - 1)
+            refetch()
+        }
+    }
+
+    const handleNext = () => {
+        if (currentPage < pages.length - 1) {
+            setCurrentPage(currentPage + 1)
+            refetch()
+        }
+    }
+
+    const handlePagination = (page) => {
+        setCurrentPage(page);
+        refetch();
+    }
+
+
     return (
         <div className="max-w-7xl mx-auto py-20">
-           <h2>All Contest</h2>           
-           <div className="grid md:grid-cols-2 gap-5">
-            {
-                publishContest.map(contest => <ContestCart key={contest._id} contest={contest} ></ContestCart> )
-            }
-           </div>
+            <h2>All Contest</h2>
+            <div className="grid md:grid-cols-2 gap-5">
+                {
+                    publishContest.map(contest => <ContestCart key={contest._id} contest={contest} ></ContestCart>)
+                }
+            </div>
+            <div className="text-center py-5">
+                <button onClick={handlePrev} className="btn">Prev</button>
+                {
+                    pages.map(page => <button key={page} onClick={() => handlePagination(page)}
+                        className={currentPage === page ? 'bg-red-600 btn text-white' : 'btn'}
+                    >{page + 1}</button>)
+                }
+                <button onClick={handleNext} className="btn">Next</button>
+            </div>
         </div>
     );
 };
